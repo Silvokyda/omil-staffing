a9ddd4b192f045a6c985ca9d5f9d4942200c2936
Silvokyda <deseve48@gmail.com>
2024-04-29 13:51:01 +0300

    white

--- components/Patners.tsx
+++ components/Patners.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Partners = () => {
   const partnerImages = [
-    '../partner1.jpg',
-    '../partner2.jpg',
-    '../partner3.jpg',
-    '../partner4.jpg',
+    '../patner1.jpg',
+    '../patner2.jpg',
+    '../patner3.jpg',
+    '../patner4.jpg',
   ];
 
   return (
     <div className="flex justify-center space-x-4 mt-8">
+        <h2 className="text-3xl font-bold mb-10 text-white ">Our Partners</h2>
       {partnerImages.map((image, index) => (
         <img key={index} src={image} alt={`Partner ${index + 1}`} className="h-24 w-auto" />
       ))}
