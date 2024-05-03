9a0ed4a1b7c3e09803e7ecc3b79a50ee99530161
Trend20 <enockomondi305@gmail.com>
2024-05-03 11:20:14 +0300

    image deployment path fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     basePath: "/omil-staffing",
     output: "export",
     reactStrictMode: true,
+    images: {
+        loader: "akamai",
+        path: "/",
+    },
+    assetPrefix: "/omil-staffing",
 };
 
 export default nextConfig;
